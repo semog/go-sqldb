2e214a87a1b03a7794697ff33fc6d35c104f136b
semog <semog@users.noreply.github.com>
2017-10-03 16:32:23 -0700

    Export PatchFuncType.

--- sqldb.go
+++ sqldb.go
@@ -15,21 +15,23 @@ type SQLDb struct {
 	*sql.DB
 }
 
-type patchFuncType struct {
-	// patchid is not necessarily sequential. It just needs to be unique.
-	patchid   int
-	patchFunc func(sdb *SQLDb) bool
+// PatchFuncType contains unique patch ID and a patch function to run.
+type PatchFuncType struct {
+	// PatchID is not necessarily sequential. It just needs to be unique, but convention is sequential.
+	PatchID int
+	// PatchFunc will perform patch operations on the database.
+	PatchFunc func(sdb *SQLDb) bool
 }
 
 // The array of patch functions that will automatically upgrade the database.
-var internalPatchDbFuncs = []patchFuncType{
+var internalPatchDbFuncs = []PatchFuncType{
 	{0, func(sdb *SQLDb) bool {
 		return sdb.CreateTable("IF NOT EXISTS version (patchid INTEGER PRIMARY KEY)")
 	}},
 }
 
 // OpenAndPatchDb - Open and Patch a database if necessary.
-func OpenAndPatchDb(dbFilename string, patchFuncs []patchFuncType) *SQLDb {
+func OpenAndPatchDb(dbFilename string, patchFuncs []PatchFuncType) *SQLDb {
 	sdb, err := OpenDb(dbFilename)
 	if err != nil {
 		return nil
@@ -52,7 +54,7 @@ func OpenDb(dbFilename string) (*SQLDb, error) {
 }
 
 // PatchDb - Patch a database if necessary.
-func (sdb *SQLDb) PatchDb(patchFuncs []patchFuncType) bool {
+func (sdb *SQLDb) PatchDb(patchFuncs []PatchFuncType) bool {
 	// Create the patch tables
 	if !sdb.patch(internalPatchDbFuncs) {
 		return false
@@ -64,23 +66,23 @@ func (sdb *SQLDb) PatchDb(patchFuncs []patchFuncType) bool {
 	return true
 }
 
-func (sdb *SQLDb) patch(patchFuncs []patchFuncType) bool {
+func (sdb *SQLDb) patch(patchFuncs []PatchFuncType) bool {
 	// Currently this patching function does not check to see when it is
 	// finished whether it is running against a _newer_ database. An additional
 	// check would need to be done to see if the final committed patchid matches the
 	// expected patchid.
 	for _, patch := range patchFuncs {
-		if !sdb.patched(patch.patchid) {
+		if !sdb.patched(patch.PatchID) {
 			if !sdb.beginPatch() {
-				log.Printf("ERROR: Could not begin patch database for version %d.\n", patch.patchid)
+				log.Printf("ERROR: Could not begin patch database for version %d.\n", patch.PatchID)
 				return false
 			}
-			if !(patch.patchFunc(sdb) && sdb.commitPatch(patch.patchid)) {
-				log.Printf("ERROR: Could not patch database for version %d.\n", patch.patchid)
+			if !(patch.PatchFunc(sdb) && sdb.commitPatch(patch.PatchID)) {
+				log.Printf("ERROR: Could not patch database for version %d.\n", patch.PatchID)
 				sdb.rollbackPatch()
 				return false
 			}
-			log.Printf("INFO: Patched database version %d.\n", patch.patchid)
+			log.Printf("INFO: Patched database version %d.\n", patch.PatchID)
 		}
 	}
 	return true
